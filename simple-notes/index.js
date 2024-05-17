f3a4aef30005f935364dbe556638379c3b06ca14
David Heidari <davidheidari2@gmail.com>
2024-05-17 11:38:53 +0200

    done with index.js

--- simple-notes/index.js
+++ simple-notes/index.js
@@ -2,24 +2,39 @@ const fs = require('fs');
 const http = require('http');
 
 const displayNotes = fs.readFileSync('index.html', 'utf-8'); 
-const displayCard = fs.readFileSync('card.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const cardTemplate = fs.readFileSync('card.html', 'utf-8');
+const notes = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 
+const noteDataToHTML = (template, note) => {
 
+    let output = template.replace('{{%TITLE%}}', note.title)
+        .replace('{{%DESCRIPTION%}}', note.description)
+        .replace('{{%DATA%}}', note.data)
+        .replace('{{%AUTHOR%}}', note.author)
+    return output;
+
+    };
 
 const server = http.createServer((request, response) => {
     const path = request.url.toLowerCase();
 
+    
+
+
     if (path === '/' || path === '/home') {
+        
+        const allNotesHtml = displayNotes.replace('{{%CONTENT%}}', notes.map((note) => {
+            noteDataToHTML(cardTemplate, note)
+        }));
         response.writeHead(200, { 'content-Type': 'text/html' });
-        response.write(displayNotes, 'utf-8', (_err) => {
+        response.write(allNotesHtml, 'utf-8', (_err) => {
             if (_err) return console.log(_err);
         });
         console.log('Welcome to home page');
         response.end();
     } else if (path === '/card.html') {
         response.writeHead(200, { 'content-Type': 'text/html' });
-        response.write(displayCard, 'utf-8', (_err) => {
+        response.write(cardTemplate, 'utf-8', (_err) => {
             if (_err) return console.log(_err);
         });
         console.log('Welcome to card page');
